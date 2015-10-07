a4ea758a46bdc1d847c24c715d40e269fa327148
Ekberjan <ekberjanderman@gmail.com>
2015-10-07 13:31:54 +0300

    Create normalizer.h

---
+++ normalizer.h
@@ -0,0 +1,28 @@
+//Header file for normalization operation
+
+#ifndef _NORMALIZER_H
+#define _NORMALIZER_H
+
+#include "commonFiles.h"
+
+class Normalizer
+{
+public:
+	cv::Mat run(cv::Mat& im, double reqmean, double reqvar);
+
+private:
+	double min;
+	double max;
+	double minNorm;
+	double maxNorm;
+	double stdNorm;
+
+	cv::Mat normalizedImage;
+	cv::Scalar mean, meanNorm;
+	cv::Point min_loc, max_loc;
+
+	float deviation(cv::Mat& im, float ave);
+
+};
+
+#endif
