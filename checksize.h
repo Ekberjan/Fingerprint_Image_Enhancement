44d10c92ac7a5f3b5eabd0647074cafe5aade966
Ekberjan <ekberjanderman@gmail.com>
2015-10-07 13:30:29 +0300

    Create checksize.h

---
+++ checksize.h
@@ -0,0 +1,17 @@
+#ifndef _CHECK_SIZE_H
+#define _CHEKC_SIZE_H
+
+#include "commonfiles.h"
+
+class CheckSize
+{
+public:
+	cv::Mat check(cv::Mat& inputImage);
+	
+private:
+	int expectedRow;
+	int expectedCol;
+	cv::Mat result;
+};
+
+#endif
