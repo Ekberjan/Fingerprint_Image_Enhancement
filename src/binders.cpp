c567209e28565902aa485a19967be65785e8b053
Julien Jerphanion (@jjerphan) <git@jjerphan.xyz>
2020-10-16 00:51:11 +0100

    Introduce Python API
    Make use of the OpenCV Mat to numpy array converter
    developed by @edmBernard:
    https://github.com/edmBernard/pybind11_opencv_numpy

--- src/binders.cpp
+++ src/binders.cpp
@@ -1,10 +1,16 @@
 #include <pybind11/pybind11.h>
 #include <string.h>
 #include "fpenhancement.h"
+#include "common.h"
+#include "ndarray_converter.h"
+
 namespace py = pybind11;
+using namespace pybind11::literals;
 
 
-PYBIND11_MODULE(finger, m) {
+PYBIND11_MODULE(fingerprint, m) {
+    NDArrayConverter::init_numpy();
+
     m.doc() = "Finger print extraction";
 
     py::class_<FPEnhancement>(m, "Extractor")
@@ -23,7 +29,26 @@ PYBIND11_MODULE(finger, m) {
                       int,    // dilationSize
                       int,    // dilationType
                       bool    // verbose
-                      >());
+                      >(),
+                      "Constructor",
+                      "kx"_a = 0.8,
+                      "ky"_a = 0.8,
+                      "block_sigma"_a = 5.0,
+                      "gradient_sigma"_a = 1.0,
+                      "orient_smooth_sigma"_a = 5.0,
+                      "freq_value"_a = 0.11,
+                      "ddepth"_a = CV_32FC1,
+                      "add_border"_a = false,
+                      "canny_low_threshold"_a = 10,
+                      "canny_ratio"_a = 3,
+                      "kernel_size"_a = 3,
+                      "blurring_times"_a = 30,
+                      "dilation_size"_a = 10,
+                      "dilation_type"_a = 1,
+                      "verbose"_a = false
+                      )
+        .def("extract_fingerprints", &FPEnhancement::extractFingerPrints)
+        .def("post_processing", &FPEnhancement::postProcessingFilter);
 }
 
 
